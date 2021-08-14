b94c6698197836d40cd00072d8478cec1c9b4092
Emmanuel Hernandez <ehernandez@athenahealth.com>
2021-08-14 15:07:43 -0500

    Added functionality to delete only .log file

--- cleanup.py
+++ cleanup.py
@@ -5,9 +5,10 @@
 """ This script will cleanup logs based on paths in the config.yml file
     or add a single file path with the --p flag"""
 
-import os
-import yaml
 import argparse
+import os
+
+import yaml
 
 CFG = 'config.yml'
 
@@ -16,21 +17,39 @@ parser.add_argument("--p",
                     dest="path",
                     help="Root path to cleanup",
                     action='store')
+parser.add_argument("--all",
+                    dest="all",
+                    help="Set this flag to delete all files in specified path.",
+                    action='store_true')
 args = parser.parse_args()
 path = args.path if args.path else None
+all = args.all if args.all else None
 
 
 def cleanup_logs(path):
+    """
+    Function takes in a file path and removes only .log files by default
+    If '--all' is set in CLI, it will delete 'ALL' files(all extension types) in the specified path
+
+    Args:
+        path ([String]): Filepath to target directory that contains files to delete
+    """
     print('[.] Removing logs in: "{}"'.format(path))
     files = os.listdir(path)
     if files:
         c = 0
         for file in files:
-            print('\t- Removing: {}'.format(file))
-            os.remove(path + file)
-            c += 1
+            if all:
+                print(f'\t- Removing all files: {file}')
+                os.remove(path + file)
+                c += 1
+            elif '.log' in file:
+                print(f'\t- Removing logs: {file}')
+                os.remove(path + file)
+                c += 1
+            else:
+                pass
         print("\n\t* Files removed: {}\n\n".format(str(c)))
-
     else:
         print("[!] No files to remove\n\n")
 
@@ -44,7 +63,6 @@ def main(path=path):
     if path:
         cleanup_logs(path)
     else:
-
         for path in paths:
             cleanup_logs(path)
 
